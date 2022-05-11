4ec3117243709f2d397476370e3aaf32df123d02
TannerB <tannerpace@gmail.com>
2022-05-11 18:29:30 -0400

    env

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@
  **/
 const nextConfig = {
   /* config options here */
+  env: {
+    description: 'app-description-its-a-blog',
+  },
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+}

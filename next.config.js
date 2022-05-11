df4a178adcf797bbb5f79586d242fe5b3fef9c02
TannerB <tannerpace@gmail.com>
2022-05-11 18:26:13 -0400

    blank next.config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+//some examples https://github.com/vercel/next.js/blob/canary/packages/next/server/config-shared.ts#L137
+
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  /* config options here */
+}
+
+module.exports = nextConfig
